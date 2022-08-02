8d7a2d799eae29c0624b59147a72f4d54604afe8
Victor Piolin <victor.piolin@gmail.com>
2022-08-01 22:18:04 -0400

    Add test Operations struct

--- domain/wallet/operation_test.go
+++ domain/wallet/operation_test.go
@@ -46,3 +46,95 @@ func TestWithProfitOutput(t *testing.T) {
 		"26225.07%",
 	}, operation.WithProfit())
 }
+
+func TestWithProfitOutputWithEmptyCurrentPrice(t *testing.T) {
+	operation := NewOperation(
+		"2022-06-19",
+		0.1,
+		"COINSUPERCOIN",
+		0,
+		"fiat",
+		10.0,
+		10.0,
+		"CAD",
+		PURCHASE,
+		"test",
+	)
+
+	assert.Equal(t, []string{
+		"COINSUPERCOIN",
+		"0.1",
+		"100",
+		"10",
+		"0",
+		"-100%",
+	}, operation.WithProfit())
+}
+
+func TestGetCurrentPriceWith0AsCurrentAssetPrice(t *testing.T) {
+	operation := NewOperation(
+		"2022-06-19",
+		0.1,
+		"BTC",
+		0,
+		"fiat",
+		10.0,
+		10.0,
+		"CAD",
+		PURCHASE,
+		"test",
+	)
+
+	assert.Equal(t, float64(0), operation.GetCurrentPrice(0))
+}
+
+func TestGetCurrentPrice(t *testing.T) {
+	operation := NewOperation(
+		"2022-06-19",
+		0.1,
+		"BTC",
+		0,
+		"fiat",
+		10.0,
+		10.0,
+		"CAD",
+		PURCHASE,
+		"test",
+	)
+
+	assert.Equal(t, float64(10), operation.GetCurrentPrice(100))
+}
+
+func TestGetCurrentUnitPriceNoPriceFound(t *testing.T) {
+	operation := NewOperation(
+		"2022-06-19",
+		0.1,
+		"COINSUPERCOIN",
+		0,
+		"fiat",
+		10.0,
+		10.0,
+		"CAD",
+		PURCHASE,
+		"test",
+	)
+
+	assert.Equal(t, float64(0), operation.getCurrentUnitPrice())
+}
+
+func TestGetCurrentUnitPriceWithPriceFound(t *testing.T) {
+	operation := NewOperation(
+		"2022-06-19",
+		0.1,
+		"BTC",
+		0,
+		"fiat",
+		10.0,
+		10.0,
+		"CAD",
+		PURCHASE,
+		"test",
+	)
+
+	assert.Equal(t, float64(26325.07), operation.getCurrentUnitPrice())
+}
